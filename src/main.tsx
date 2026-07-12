53041f7ac1567ac4f426cbafd63e6a8133588bb3
catchybee <deepakrameshwarm@gmail.com>
2026-07-12 23:36:26 +0530

    fix: update main.tsx provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ConvexReactClient } from "convex/react";
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ConvexProvider client={convex}>
-      <ConvexAuthProvider client={convex}>
-        <App />
-      </ConvexAuthProvider>
-    </ConvexProvider>
+    <ConvexAuthProvider client={convex}>
+      <App />
+    </ConvexAuthProvider>
   </React.StrictMode>
 );
